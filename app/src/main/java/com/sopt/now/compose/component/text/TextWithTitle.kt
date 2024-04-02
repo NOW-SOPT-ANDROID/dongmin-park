f4a8af6de9a6f6a337b2a71198dc3c7a9e4fa62c
Dongmin <pdm001125@naver.com>
2024-04-02 20:18:32 +0900

    feat #2: TextWithTitle 컴포넌트 구현

---
+++ app/src/main/java/com/sopt/now/compose/component/text/TextWithTitle.kt
@@ -0,0 +1,36 @@
+package com.sopt.now.compose.component.text
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TextWithTitle(title: String, description: String) {
+    Column {
+        Text(
+            text = title,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 30.sp
+        )
+        Spacer(modifier = Modifier.padding(vertical = 4.dp))
+        Text(
+            text = description,
+            fontWeight = FontWeight.Light,
+            color = Color.Gray
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TextWithTitlePreview() {
+    TextWithTitle("ID", "chattymin")
+}
