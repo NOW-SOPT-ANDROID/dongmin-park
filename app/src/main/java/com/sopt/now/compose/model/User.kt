69677c0047faebe9c23c5d2c24b5281384992f87
Dongmin <pdm001125@naver.com>
2024-04-22 18:51:20 +0900

    feat #6: 무의미한 Parcelize 제거

--- app/src/main/java/com/sopt/now/compose/model/User.kt
+++ app/src/main/java/com/sopt/now/compose/model/User.kt
@@ -1,11 +1,8 @@
 package com.sopt.now.compose.model
 
-import android.os.Parcelable
 import androidx.annotation.DrawableRes
 import com.sopt.now.compose.R
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 data class User(
     val id: String = "",
     val pw: String = "",
@@ -13,4 +10,4 @@ data class User(
     val juryang: String = "",
     @DrawableRes val profileImage: Int = R.drawable.img_profile,
     val selfDescription: String = "하이요"
-) : Parcelable
+)
