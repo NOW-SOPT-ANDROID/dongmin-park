898289b757070a1f810ad1985927663ed4a6b559
Dongmin <pdm001125@naver.com>
2024-04-23 17:23:49 +0900

    fix/#6 : DataStoreImpl orEmpty 적용

--- app/src/main/java/com/sopt/now/compose/data/local/UserDataStoreImpl.kt
+++ app/src/main/java/com/sopt/now/compose/data/local/UserDataStoreImpl.kt
@@ -8,16 +8,16 @@ class UserDataStoreImpl @Inject constructor(
     private val dataStore: SharedPreferences,
 ) : UserDataStore {
     override var id: String
-        get() = dataStore.getString(ID, "") ?: ""
+        get() = dataStore.getString(ID, "").orEmpty()
         set(value) = dataStore.edit { putString(ID, value) }
     override var pw: String
-        get() = dataStore.getString(PW, "") ?: ""
+        get() = dataStore.getString(PW, "").orEmpty()
         set(value) = dataStore.edit { putString(PW, value) }
     override var nickname: String
-        get() = dataStore.getString(NICKNAME, "") ?: ""
+        get() = dataStore.getString(NICKNAME, "").orEmpty()
         set(value) = dataStore.edit { putString(NICKNAME, value) }
     override var juryang: String
-        get() = dataStore.getString(JURYANG, "") ?: ""
+        get() = dataStore.getString(JURYANG, "").orEmpty()
         set(value) = dataStore.edit { putString(JURYANG, value) }
 
     override fun clearInfo() {
