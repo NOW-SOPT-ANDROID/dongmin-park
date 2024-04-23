26fec288169e621812b9ed71b6b17d2a3bca75e5
Dongmin <pdm001125@naver.com>
2024-04-24 02:37:37 +0900

    fix/#5 : savedStateHandler 적용

--- app/src/main/java/com/sopt/now/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/sopt/now/presentation/main/MainViewModel.kt
@@ -1,15 +1,22 @@
 package com.sopt.now.presentation.main
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.sopt.now.data.local.mockProfileList
 import com.sopt.now.model.User
 
-class MainViewModel : ViewModel() {
-    lateinit var userInfo: User
+class MainViewModel(val savedStateHandle: SavedStateHandle) : ViewModel() {
+    var userInfo: User?
+        get() = savedStateHandle.get<User>(USER)
+        set(value) = savedStateHandle.set(USER, value)
 
     fun setUserInfoValue(user: User) {
         userInfo = user
     }
 
     fun getMockProfileList() = mockProfileList
+
+    companion object {
+        val USER = "user"
+    }
 }
