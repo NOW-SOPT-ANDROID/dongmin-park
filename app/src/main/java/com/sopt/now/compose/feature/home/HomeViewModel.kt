48e826fac95d6989c135bcb1291c950d6861c4d9
Dongmin <pdm001125@naver.com>
2024-05-02 04:20:10 +0900

    fix/#10 : change blank

--- app/src/main/java/com/sopt/now/compose/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/sopt/now/compose/feature/home/HomeViewModel.kt
@@ -1,9 +1,7 @@
 package com.sopt.now.compose.feature.home
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.sopt.now.compose.data.local.UserDataStore
 import com.sopt.now.compose.domain.repository.ReqresRepository
 import com.sopt.now.compose.util.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,7 +13,6 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val userDataStore: UserDataStore,
     private val reqresRepository: ReqresRepository,
 ) : ViewModel() {
     private val _state: MutableStateFlow<HomeState> = MutableStateFlow(HomeState())
