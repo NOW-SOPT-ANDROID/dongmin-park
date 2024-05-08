863891da9cb5751d21095831f59e74d8e4d1fe3a
Dongmin <pdm001125@naver.com>
2024-05-08 16:58:46 +0900

    fix/#9 : 네이밍 변경

--- app/src/main/java/com/sopt/now/App.kt
+++ app/src/main/java/com/sopt/now/App.kt
@@ -1,6 +1,8 @@
 package com.sopt.now
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import com.sopt.now.compose.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -10,9 +12,14 @@ class App : Application() {
         super.onCreate()
 
         initTimber()
+        setDayMode()
     }
 
     private fun initTimber() {
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+    private fun setDayMode() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
